07d242ffc75ee94a5c902a8ff510937d1a0ed902
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2023-05-18 16:46:46 +0300

    Choose column of depth one

--- game2.py
+++ game2.py
@@ -100,19 +100,38 @@ def insertPiece(board, column, piece):
     return newBoard
 
 
-def minMax(board, minOrMax, maxDepth, currentDepth=0):
+def minMax(board, minOrMax, maxDepth, currentDepth=1):
     if currentDepth == maxDepth:
         return getUtility(board)
     arr = []
     for i in range(0, 7):
         if minOrMax == "max":
             newBoard = insertPiece(board, i, 'R')
-            currentUtility = minMax(newBoard, currentDepth + 1, "min")
+            currentUtility = minMax(newBoard, "min", maxDepth, currentDepth + 1)
             arr.append(currentUtility)
         else:
             newBoard = insertPiece(board, i, 'B')
-            currentUtility = minMax(newBoard, currentDepth + 1, ",max")
+            currentUtility = minMax(newBoard, "max", maxDepth, currentDepth + 1)
             arr.append(currentUtility)
+    arr = sorted(arr)
+    #print(arr)
+    if minOrMax == "max":
+        return arr[-1]
+    else:
+        return arr[0]
+
+
+def chooseColumn(board, maxDepth):
+    arr = []
+    for i in range(0, 7):
+        #printBoard(board)
+        newBoard = insertPiece(board, i, 'R')
+        currentUtility = minMax(newBoard, "min", maxDepth)
+        pair = (currentUtility, i)
+        arr.append(pair)
+    arr = sorted(arr)
+    print(arr)
+    return arr[-1][1]
 
 
 def test():
@@ -121,7 +140,19 @@ def test():
     pair3 = (4, -2)
     arr = [pair, pair2, pair3]
     arr = sorted(arr)
-    print(arr)
+    print(arr[-1][0])
+
+
+def printBoard(board):
+    print(" ", end="")
+    for i in range(0, 7):
+        print(" ", i, end="")
+    print()
+    for i in range(0, len(board)):
+        print(i, " ", end="")
+        for j in range(0, len(board[i])):
+            print(board[i][j], end="  ")
+        print()
 
 def main():
     # board = Board()
@@ -131,10 +162,9 @@ def main():
     # for k, l in zip(range(i, i+4), reversed(range(j-3, j+1))):
     # print(k, l)
 
-    # b.board=insertPeice(b.board, 5, 'B')
+    # b.board=insertPiece(b.board, 5, 'B')
     # b.print_grid()
-    test()
-    """
+    #test()
     while True:
         x = 0
         y = 0
@@ -143,10 +173,11 @@ def main():
         y = int(input())
         z = input()
         b.board[x][y] = z
-        b.board = insertPeice(b.board, 5, 'B')
+        # b.board = insertPiece(b.board, 5, 'B')
         b.print_grid()
         print(getUtility(b.board))
-    """
+        #minMax(b.board, "max", 1)
+        #print(chooseColumn(b.board, 1))
 
     # time.sleep(2)
     # game_end = False
