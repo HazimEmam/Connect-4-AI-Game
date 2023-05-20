adddc5a8c333f80a034bf49e382f41df98bb3616
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2023-05-20 20:23:20 +0300

    Application Running

--- game.py
+++ game.py
@@ -1,26 +1,20 @@
 from board import Board
 import time
 import random
+from GUITemp import GUIGame
 from alphaBeta import AlphaBeta
 from MinMax import MinMax
-#from MinMax import MinMax
 
 # GAME LINK
 # http://kevinshannon.com/connect4/
 
-
 def main():
+    
     board = Board()
-    #(game_board, game_end) = board.get_game_grid()
-    #board.print_grid(game_board)
     alpha = AlphaBeta()
     min = MinMax()
-    #check =alpha.alphaBetaPruning(game_board,"max",1)
-    #board.print_grid(game_board)
-    #print(check)
-    #col = alpha.chooseColumn(game_board,3)
-    #print(col)
     
+    Gui = GUIGame()
     time.sleep(2)
     game_end = False
     while not game_end:
@@ -28,14 +22,14 @@ def main():
 
         # FOR DEBUG PURPOSES
         board.print_grid(game_board)
-        #time.sleep(2)
+        time.sleep(2)
 
         # YOUR CODE GOES HERE
 
         # Insert here the action you want to perform based on the output of the algorithm
         # You can use the following function to select a column
         selected_column = alpha.chooseColumn(game_board,4)
-        print("selected column = ",selected_column)
+        print("selected :" , selected_column)
         board.select_column(selected_column)
 
         time.sleep(2)
